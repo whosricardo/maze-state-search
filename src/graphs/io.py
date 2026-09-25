5651985416f68690f260a6bb531dcf2efac482ee
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2026-09-24 23:29:11 -0300

    feat: add verification to validate if the matrix is a rectangle
    [Description]
    implemented a function to verify if the matrix
    parsed is actualy a valid rectangle
    
    [Solution]
    implemented validate_rectangle() that receives
    the parsed grid and uses the first row len
    to validate the other rows in the grid

--- src/graphs/io.py
+++ src/graphs/io.py
@@ -3,3 +3,17 @@ def parse_map(path):
         map_data = [list(line.strip()) for line in file]
 
     return map_data
+
+
+def validate_map(grid):
+    if not validate_rectangular(grid):
+        return False
+
+    return True
+
+
+def validate_rectangular(grid):
+    if not grid:
+        return True
+    row_len = len(grid[0])
+    return all(len(row) == row_len for row in grid)
